c97403488f7e898da4775c169e24c2cd2d54d95e
brillejesus <alf.lokken@intility.no>
2024-02-09 12:49:35 +0100

    ensure query params are urlencoded

--- src/Invoke-OAuth2AuthorizationEndpoint.ps1
+++ src/Invoke-OAuth2AuthorizationEndpoint.ps1
@@ -12,10 +12,10 @@ function Invoke-OAuth2AuthorizationEndpoint {
     Authorization endpoint URL.
 
     .PARAMETER client_id
-    The identifier of the client at the authorisation server.
+    The identifier of the client at the authorization server.
 
     .PARAMETER redirect_uri
-    The client callback URI for the authorisation response.
+    The client callback URI for the authorization response.
 
     .PARAMETER response_type
     Tells the authorization server which grant to execute. Default is code.
@@ -30,22 +30,20 @@ function Invoke-OAuth2AuthorizationEndpoint {
     OPTIONAL Informs the Authorization Server of the mechanism to be used for returning Authorization Response. Determined by response_type, if not specified.
 
     .PARAMETER customParameters
-    Hashtable with custom parameters to be added to the request uri (e.g. domain_hint, prompt, etc.)
+    Hashtable with custom parameters to be added to the request uri (e.g. domain_hint, prompt, etc.) both the key and value will be url encoded.
 
     .EXAMPLE
     PS> Invoke-OAuth2AuthorizationEndpoint -uri "https://acc.spotify.com/authorize" -client_id "2svXwWbFXj" -scope "user-read-currently-playing" -redirect_uri "http://localhost"
-        
     code_verifier                  xNTKRgsEy_u2Y.PQZTmUbccYd~gp7-5v4HxS7HVKSD2fE.uW_yu77HuA-_sOQ...
     redirect_uri                   https://localhost
     client_id                      2svXwWbFXj
     code                           AQDTWHSP6e3Hx5cuJh_85r_3m-s5IINEcQZzjAZKdV4DP_QRqSHJzK_iNB_hN...
 
-    A request for user authorization is sent to the /authorize endpoint along with a code_challenge, code_challenge_method and state param. If successful, the authorization server 
-    will redirect back to the redirect_uri with a code which can be exchanged for an access token.
+    A request for user authorization is sent to the /authorize endpoint along with a code_challenge, code_challenge_method and state param. 
+    If successful, the authorization server will redirect back to the redirect_uri with a code which can be exchanged for an access token.
     
     .EXAMPLE
     PS> Invoke-OAuth2AuthorizationEndpoint -uri "https://example.org/oauth2/authorize" -client_id "0325" -redirect_uri "http://localhost" -scope "user.read" -response_type "token" -usePkce:$false -customParameters @{ login = "none" }
-
     expires_in                     4146
     expiry_datetime                01.02.2024 10:56:06
     scope                          User.Read profile openid email
@@ -53,7 +51,7 @@ function Invoke-OAuth2AuthorizationEndpoint {
     access_token                   eyJ0eXAiQiJKV1QiLCJub25jZSI6InAxYTlHksH6bktYdjhud3VwMklEOGtUM...
     token_type                     Bearer
 
-    Implicit grant flow
+    Implicit Grant, will return a access_token if successful.
     #>
     [Alias('Invoke-AuthorizationEndpoint','authorize')]
     [OutputType([hashtable])]
@@ -85,11 +83,28 @@ function Invoke-OAuth2AuthorizationEndpoint {
         [hashtable]$customParameters
     )
 
-    # Arbitrary string value to be verified in the response, prevent cross-site request forgery (CSRF) (can also be used to uniquely identify a session).
-    [string]$state = Get-RandomString -Length ( (16..21) | get-random )
+    # Determine which protocol is being used.
+    if ( $response_type -eq "token" -or ($response_type -match "^code$" -and $scope -notmatch "openid" ) ) { 
+        $protocol = "OAUTH" 
+        $nonce = $null
+    }
+    else { $protocol = "OIDC"
+        # ensure scope contains openid for oidc flows
+        if ( $scope -notmatch "openid" ) { Write-Warning "Invoke-OAuth2AuthorizationRequest: Added openid scope to request (OpenID requirement)."; $scope += " openid" }
+        # generate nonce for id_token validation
+        [string]$nonce = Get-RandomString -Length ( (32..64) | get-random )
+        Write-Verbose "Invoke-OAuth2AuthorizationRequest: adding nonce $nonce to request (OIDC flow)."
+    }
 
-    # build request uri
-    $uri += "?response_type=$([System.Web.HttpUtility]::UrlEncode($response_type))&client_id=$client_id&state=$state"
+    # generate state for CSRF protection (optional, but recommended)
+    [string]$state = Get-RandomString -Length ( (16..21) | get-random )
+    
+    # begin building the request uri
+    Add-Type -AssemblyName System.Web -ErrorAction Stop
+    $uri += "?response_type=$([System.Web.HttpUtility]::UrlEncode($response_type))&client_id=$([System.Web.HttpUtility]::UrlEncode($client_id))&state=$([System.Web.HttpUtility]::UrlEncode($state))"
+    if ( $redirect_uri ) { $uri += "&redirect_uri=$([System.Web.HttpUtility]::UrlEncode($redirect_uri))" } 
+    if ( $scope ) { $uri += "&scope=$([System.Web.HttpUtility]::UrlEncode($scope))" }
+    if ( $nonce ) { $uri += "&nonce=$([System.Web.HttpUtility]::UrlEncode($nonce))" }
     
     # PKCE for code flows
     if ( $response_type -notmatch "code" -and $usePkce ) { write-verbose "Invoke-OAuth2AuthorizationRequest: PKCE is not supported for implicit flows." }
@@ -100,33 +115,17 @@ function Invoke-OAuth2AuthorizationEndpoint {
         }
     }
 
-    # Determine protocol being used.
-    if ( $response_type -eq "token" -or ($response_type -match "^code$" -and $scope -notmatch "openid" ) ) { 
-        $protocol = "OAUTH" 
-        $nonce = $null
-    }
-    else { $protocol = "OIDC"
-        # ensure scope contains openid for oidc flows
-        if ( $scope -notmatch "openid" ) { Write-Warning "Invoke-OAuth2AuthorizationRequest: Added openid scope to request (OpenID requirement)."; $scope += " openid" }
-        # generate nonce for id_token validation
-        [string]$nonce = Get-RandomString -Length ( (32..64) | get-random ); $uri += "&nonce=$nonce"
-        Write-Verbose "Invoke-OAuth2AuthorizationRequest: adding nonce $nonce to request (OIDC flow)."
-    }
-
-    # Add scope and redirect_uri to request uri
-    if ( $redirect_uri -or $scope ) { 
-        Add-Type -AssemblyName System.Web -ErrorAction Stop
-        if ( $redirect_uri ) { $uri += "&redirect_uri=$([System.Web.HttpUtility]::UrlEncode($redirect_uri))" } 
-        if ( $scope ) { $uri += "&scope=$([System.Web.HttpUtility]::UrlEncode($scope))" }
-    }
-
     # Add custom parameters to request uri
     if ( $customParameters ) { 
-        foreach ( $key in $customParameters.Keys ) { $uri += "&$key=$($customParameters[$key])" }
+        foreach ( $key in $customParameters.Keys ) { 
+            $urlEncodedValue = [System.Web.HttpUtility]::UrlEncode($customParameters[$key])
+            $urlEncodedKey = [System.Web.HttpUtility]::UrlEncode($key)
+            $uri += "&$urlEncodedKey=$urlEncodedValue"
+        }
     }
     Write-Verbose "Invoke-OAuth2AuthorizationRequest $protocol request uri $uri"
     
-    # form_post response_mode: start http.sys listener as job and give it some time to start
+    # if form_post: start http.sys listener as job and give it some time to start
     if ( $response_mode -eq "form_post" ) { 
         $uri += "&response_mode=form_post"
         $job = Start-Job -ScriptBlock (Get-Command 'New-HttpListener').ScriptBlock -ArgumentList $redirect_uri
@@ -136,7 +135,7 @@ function Invoke-OAuth2AuthorizationEndpoint {
     # authorization request (interactive)
     $webSource = Invoke-WebView2 -uri $uri -UrlCloseConditionRegex "$redirect_uri|error=[^&]*|code=[^&]*" -title "Authorization code flow"
     
-    # job should be running (awaiting request) if no error occured
+    # if form post - retreive job (post) after interaction has been complete
     if ( $response_mode -eq "form_post" ) {
         Write-Verbose "Invoke-OAuth2AuthorizationRequest: http.sys waiting for form_post request. (timeout 10s)"
         Wait-Job $job -Timeout 10 | Out-Null
@@ -144,7 +143,6 @@ function Invoke-OAuth2AuthorizationEndpoint {
             Write-Verbose "Invoke-OAuth2AuthorizationRequest: http.sys did not receive form_post request before timeout (10s)."; Stop-Job $job; Remove-Job $job
             throw "Invoke-OAuth2AuthorizationRequest: did not receive form_post request before timeout (10s)."
         }
-
         try { $jobData = Receive-Job $job -ErrorAction Stop }
         catch { 
             if ( $_.Exception.Message -match "Access is denied." ) { throw "Unabled to start http.sys listener, please run as admin." }
@@ -155,8 +153,7 @@ function Invoke-OAuth2AuthorizationEndpoint {
         $webSource = @{ Fragment = $jobData; Query = $null }
     }
 
-    # When the window closes, the script will continue and retreive the auth code from the url source
-    # Token retreival dependent on response_mode
+    # When the window closes (WebView2), the script will continue and retreive the depending on the response_mode and content.
     if( $webSource.query -match "code=" ) {
         $response = @{}
         $response.code = [System.Web.HttpUtility]::ParseQueryString($webSource.Query)['code']
@@ -187,7 +184,7 @@ function Invoke-OAuth2AuthorizationEndpoint {
     }
     else { throw "invalid response received" }
 
-    # if code flow, add client_id, code_verifier and redirect_uri to response (needed for token exchange) 
+    # if code grant, add client_id, code_verifier and redirect_uri to response (needed for token exchange) 
     if ( $response_type -match "code" ) {
         $response.client_id = $client_id
         if ( $usePkce ) { $response.code_verifier = $pkce.code_verifier }
@@ -198,19 +195,19 @@ function Invoke-OAuth2AuthorizationEndpoint {
     if ( $response.state -ne $state ) { throw "State mismatch!`nExpected '$state', got '$($response.state)'." }
     else { Write-Verbose "Invoke-OAuth2AuthorizationRequest: Validated state in response." }
 
-    # validate id_token nonce
+    # if OIDC - validate id_token nonce
     if ( $nonce -and $response.ContainsKey("id_token") ) { 
         $decodedToken = ConvertFrom-JsonWebToken $response.id_token -Verbose:$false
         if ( $decodedToken.nonce -ne $nonce ) { throw "id_token nonce mismatch`nExpected '$nonce', got '$($decodedToken.nonce)'." }
         Write-Verbose "Invoke-OAuth2TokenExchange: Validated nonce in id_token response."
     }
 
-    # add expiry datetime to response
+    # add expiry_datetime to the response
     if ( $response.ContainsKey("expires_in") ) { 
         $response["expires_in"] = [int]$response["expires_in"]
         $response.expiry_datetime = (get-date).AddSeconds($response.expires_in) 
     }
-    
+
     # remove state from response
     if ( $response.ContainsKey("state") ) { $response.Remove("state") }
 
